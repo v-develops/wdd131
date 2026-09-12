3dc15725a77258891692849594eb8ea871d191ab
v-develops <andradevcc@gmail.com>
2026-09-12 00:22:08 -0300

    Home Page - Finished

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.querySelector("#current-year").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = document.lastModified;
